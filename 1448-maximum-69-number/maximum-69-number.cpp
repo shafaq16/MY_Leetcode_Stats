d2bd658bd7f1551cf91762676015542b4450bdf8
Shafaqun Nisa <nisashafaqun@gmail.com>
2025-08-16 20:47:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (41.82%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        string temp = to_string(num);
-        for(int i = 0; i<temp.size(); i++){
-            if(temp[i]=='6'){
-                temp[i] = '9';
-                break;
-            }
-        }
-
-        return num = stoi(temp);
+       int pos = -1, cnt = 0, temp = num;
+        while (temp > 0) {
+        if (temp % 10 == 6)  pos = cnt;
+        temp /= 10;
+        cnt++;
+    }
+       return (pos==-1)?num: int(num + (3*pow(10,pos)));
     }
 };
+
+
+// class Solution {
+// public:
+//     int maximum69Number (int num) {
+//         string temp = to_string(num);
+//         for(int i = 0; i<temp.size(); i++){
+//             if(temp[i]=='6'){
+//                 temp[i] = '9';
+//                 break;
+//             }
+//         }
+
+//         return num = stoi(temp);
+//     }
+// };
