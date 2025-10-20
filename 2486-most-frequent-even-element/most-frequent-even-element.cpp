3175e8f00a94b7932583313624426c89799a5fee
Shafaqun Nisa <nisashafaqun@gmail.com>
2025-10-21 02:18:48 +0530

    Time: 23 ms (67.25%) | Memory: 41.7 MB (79.47%) - LeetSync

---
+++ 2486-most-frequent-even-element/most-frequent-even-element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+        int n = nums.size();
+        int ans = -1;
+        unordered_map<int,int> mp;
+        for(int i = 0; i<n; i++){
+            if(nums[i]%2 ==0){
+                mp[nums[i]]++;
+            }
+        }
+        int maxfreq = 0;
+        for(auto& p: mp){
+            if(maxfreq < p.second){
+                maxfreq = p.second;
+                ans = p.first;
+            }
+            else if(maxfreq == p.second){
+                ans = min(ans,p.first);
+            }
+        }
+        return ans;
+    }
+};
