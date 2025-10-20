51b066da656d21486414528c28d133b1f429ce0c
Shafaqun Nisa <nisashafaqun@gmail.com>
2025-10-21 02:33:57 +0530

    Time: 18 ms (81.59%) | Memory: 41.7 MB (88.21%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.cpp
+++ 2486-most-frequent-even-element/most-frequent-even-element.cpp
@@ -1,26 +1,48 @@
+// class Solution {
+// public:
+//     int mostFrequentEven(vector<int>& nums) {
+//         int n = nums.size();
+//         int ans = -1;
+//         unordered_map<int,int> mp;
+//         for(int i = 0; i<n; i++){
+//             if(nums[i]%2 ==0){
+//                 mp[nums[i]]++;
+//             }
+//         }
+//         int maxfreq = 0;
+//         for(auto& p: mp){
+//             if(maxfreq < p.second){
+//                 maxfreq = p.second;
+//                 ans = p.first;
+//             }
+//             else if(maxfreq == p.second){
+//                 if(p.first<ans){
+//                     ans=p.first;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
-    int mostFrequentEven(vector<int>& nums) {
-        int n = nums.size();
+    int mostFrequentEven(std::vector<int>& nums) {
         int ans = -1;
-        unordered_map<int,int> mp;
-        for(int i = 0; i<n; i++){
-            if(nums[i]%2 ==0){
-                mp[nums[i]]++;
-            }
-        }
-        int maxfreq = 0;
-        for(auto& p: mp){
-            if(maxfreq < p.second){
-                maxfreq = p.second;
-                ans = p.first;
-            }
-            else if(maxfreq == p.second){
-                if(p.first<ans){
-                    ans=p.first;
+        int freq = 0;
+        unordered_map<int, int> mp;
+
+        for (int i : nums) {
+            if (i % 2 == 0) {
+                mp[i]++;
+                if (mp[i] > freq || (mp[i] == freq && i < ans)) {
+                    ans = i;
+                    freq = mp[i];
                 }
             }
         }
-        return ans;
+
+        return freq==0? -1: ans;
     }
 };
