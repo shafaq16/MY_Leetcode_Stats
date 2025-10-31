2ded3ee1f4b08836b211b93fb310392bfcf84913
Shafaqun Nisa <nisashafaqun@gmail.com>
2025-10-31 16:16:45 +0530

    Time: 0 ms (100.00%) | Memory: 25.3 MB (75.61%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,14 +1,38 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<int> ans;
-        for(int i = 1; i<nums.size(); i++){
-             if(nums[i]==nums[i-1]) ans.push_back(nums[i]);
+       int n = nums.size()-2;
+        int a=0,b=0, Xor = 0;
+        for(int i = 0; i<nums.size() ;i++){
+            Xor ^= nums[i]^(i<n?i:0);
+       }
+        int diffBit = Xor & -Xor;
+        for (int num : nums) {
+            if ((num & diffBit) == 0) a ^= num;
+            else b ^= num;
         }
-        return ans;
+        for (int i = 0; i < n; i++) {
+            if ((i & diffBit) == 0) a ^= i;
+            else b ^= i;
+        }
+        return {a,b};
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> getSneakyNumbers(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         vector<int> ans;
+//         for(int i = 1; i<nums.size(); i++){
+//              if(nums[i]==nums[i-1]) ans.push_back(nums[i]);
+//         }
+//         return ans;
+//     }
+// };
 
 
 
