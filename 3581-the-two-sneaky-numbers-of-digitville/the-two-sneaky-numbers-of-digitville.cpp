004120e53ada2d189099f2094399084360e83947
Shafaqun Nisa <nisashafaqun@gmail.com>
2025-10-31 16:05:14 +0530

    Time: 3 ms (40.61%) | Memory: 25.3 MB (86.35%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         vector<int> ans;
-        unordered_map<int,int> mp;
-        for(int i: nums){
-            if(mp[i] == 1){
-                ans.push_back(i);
-            }
-            mp[i]++;
+        for(int i = 1; i<nums.size(); i++){
+             if(nums[i]==nums[i-1]) ans.push_back(nums[i]);
         }
         return ans;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> getSneakyNumbers(vector<int>& nums) {
+//         vector<int> ans;
+//         unordered_map<int,int> mp;
+//         for(int i: nums){
+//             if(mp[i] == 1){
+//                 ans.push_back(i);
+//             }
+//             mp[i]++;
+//         }
+//         return ans;
+//     }
+// };
