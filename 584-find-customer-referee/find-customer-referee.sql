e023229eb2e2b9bdfa7dbf62dd0bc14aae447f42
Shafaqun Nisa <nisashafaqun@gmail.com>
2025-11-04 03:35:26 +0530

    Time: 440 ms (67.90%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,19 +1,24 @@
 # Write your MySQL query statement below
 
-#METHOD 2 - NESTED
-SELECT  
-    name
-FROM 
-    Customer
-WHERE 
-    id NOT IN
-        (
-        SELECT id
-        FROM Customer
-        WHERE referee_id = 2
-        )
+-- #METHOD 3 - IFNULL(expression,Alt_value) function;
+SELECT name FROM Customer WHERE IFNULL(referee_id,0) <> 2;
 
 
 
--- #METHOD 1
--- SELECT name FROM Customer WHERE referee_id != 2 OR referee_id is NULL;
+-- #METHOD 2 - NESTED
+-- SELECT  
+--     name
+-- FROM 
+--     Customer
+-- WHERE 
+--     id NOT IN
+--         (
+--         SELECT id
+--         FROM Customer
+--         WHERE referee_id = 2
+--         )
+
+
+
+-- -- #METHOD 1
+-- -- SELECT name FROM Customer WHERE referee_id != 2 OR referee_id is NULL;
