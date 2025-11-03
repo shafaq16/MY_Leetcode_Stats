8497ad8851bf892f7b5ee06a3a41f9f7e7690409
Shafaqun Nisa <nisashafaqun@gmail.com>
2025-11-04 03:25:44 +0530

    Time: 410 ms (93.77%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,2 +1,19 @@
 # Write your MySQL query statement below
-SELECT name FROM Customer WHERE referee_id != 2 OR referee_id is NULL;
+
+#METHOD 2 - NESTED
+SELECT  
+    name
+FROM 
+    Customer
+WHERE 
+    id NOT IN
+        (
+        SELECT id
+        FROM Customer
+        WHERE referee_id = 2
+        )
+
+
+
+-- #METHOD 1
+-- SELECT name FROM Customer WHERE referee_id != 2 OR referee_id is NULL;
