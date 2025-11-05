3ad0926fac36951ce032ec81cc138dbb1499629f
Shafaqun Nisa <nisashafaqun@gmail.com>
2025-11-06 01:05:58 +0530

    Time: 676 ms (16.25%) | Memory: 0B (100.00%) - LeetSync

--- 1827-invalid-tweets/invalid-tweets.sql
+++ 1827-invalid-tweets/invalid-tweets.sql
@@ -1,4 +1,21 @@
-# Write your MySQL query statement below
+# Method 1
+
+
+-- SELECT tweet_id
+-- FROM Tweets
+-- WHERE CHAR_LENGTH(content) > 15
+
+
+
+
+
+
+
+
+-- MTHOD -2 That run only in this test cases as Length returns value in bytes so if other special character will be used like € it will return 3
+-- example SELECT LENGTH('€')  # is equal to 3
+--         SELECT CHAR_LENGTH('€') # is equal to 1
+
 SELECT tweet_id
 FROM Tweets
-WHERE CHAR_LENGTH(content) > 15
+WHERE LENGTH(content) > 15
