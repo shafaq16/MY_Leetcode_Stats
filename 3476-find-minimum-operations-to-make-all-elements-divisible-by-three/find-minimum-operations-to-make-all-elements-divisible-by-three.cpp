bd59e16252f708dba3bb3b0469cc090a1331bbf5
Shafaqun Nisa <nisashafaqun@gmail.com>
2025-11-22 21:38:36 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (45.48%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
+        int n = nums.size();
         int cnt = 0;
-        for(int i = 0; i<nums.size(); i++){
-            if(nums[i]%3==0) continue;
-            else cnt++; 
+        for(int i: nums){
+            if(i%3 == 0) cnt++;
         }
-        return cnt;
+        return n-cnt;
     }
 };
+
+
+// method 2
+// class Solution {
+// public:
+//     int minimumOperations(vector<int>& nums) {
+//         int cnt = 0;
+//         for(int i = 0; i<nums.size(); i++){
+//             if(nums[i]%3==0) continue;
+//             else cnt++; 
+//         }
+//         return cnt;
+//     }
+// };
