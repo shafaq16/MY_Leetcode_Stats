bc6d2afb01a79402b07919741db91a39ee46144e
Shafaqun Nisa <nisashafaqun@gmail.com>
2025-11-24 20:47:50 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (37.78%) - LeetSync

--- 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.cpp
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.cpp
@@ -1,21 +1,37 @@
+// class Solution {
+// public:
+//     vector<bool> prefixesDivBy5(vector<int>& nums) {
+//         vector<bool> ans;
+//         string s;
+//         for(int i = 0; i<nums.size(); i++){
+//            s += char('0' + nums[i]);
+
+//             int rem = 0;
+//             for(char c: s){
+//                 rem = (rem*2 + (c-'0')) % 5;     
+//             }
+//             if(rem == 0){
+//                     ans.push_back(true);
+//                 }
+//                 else{
+//                     ans.push_back(false);
+//                 }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+//  OPTIMAL APPROACH
 class Solution {
 public:
     vector<bool> prefixesDivBy5(vector<int>& nums) {
         vector<bool> ans;
-        string s;
-        for(int i = 0; i<nums.size(); i++){
-           s += char('0' + nums[i]);
-
-            int rem = 0;
-            for(char c: s){
-                rem = (rem*2 + (c-'0')) % 5;     
-            }
-            if(rem == 0){
-                    ans.push_back(true);
-                }
-                else{
-                    ans.push_back(false);
-                }
+        int rem = 0;
+        for (int v : nums) {
+            rem = (rem * 2 + v) % 5;
+            ans.push_back(rem == 0);
         }
         return ans;
     }
