6149b2c097985fea4bc3355b7cb242fc08a6b1c1
Shafaqun Nisa <nisashafaqun@gmail.com>
2025-12-08 00:33:26 +0530

    Time: 3 ms (32.99%) | Memory: 11.8 MB (46.91%) - LeetSync

--- 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.cpp
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.cpp
@@ -1,17 +1,31 @@
+// class Solution {
+// public:
+//     string removeTrailingZeros(string num) {
+//         int idx;
+//         for(int i = num.length()-1; i>=0; i--){
+//             if(num[i]!= '0'){
+//                 idx = i;
+//                 break;
+//             }
+//         }
+//         string ans ="";
+//         for(int i=0; i<=idx; i++){
+//             ans += num[i];
+//         }
+//         return  ans;
+//     }
+// };
+
 class Solution {
 public:
     string removeTrailingZeros(string num) {
-        int idx;
-        for(int i = num.length()-1; i>=0; i--){
-            if(num[i]!= '0'){
+        int idx = -1;
+        for(int i = num.length() - 1; i >= 0; i--){
+            if(num[i] != '0'){   // fix
                 idx = i;
                 break;
             }
         }
-        string ans ="";
-        for(int i=0; i<=idx; i++){
-            ans += num[i];
-        }
-        return  ans;
+        return num.substr(0, idx + 1);  // simpler extraction
     }
 };
