eaef2541683679922ab8c31242cb3bf6b5bd2dc5
Shafaqun Nisa <nisashafaqun@gmail.com>
2025-12-20 23:48:22 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (45.93%) - LeetSync

---
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& s) {
+        
+        int ans = 0;
+        for(int i = 0; i<s[0].size(); i++){
+            for(int j = 1; j<s.size(); j++){
+                if (s[j][i] < s[j-1][i]){
+                    ans ++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
