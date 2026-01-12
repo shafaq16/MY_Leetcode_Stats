25632faeeacfcb13dcaf5e849809a1d363af5410
Shafaqun Nisa <nisashafaqun@gmail.com>
2026-01-13 00:19:11 +0530

    Time: 4 ms (6.23%) | Memory: 87.1 MB (72.71%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+         int cnt5 = 0, cnt10 = 0;
+        for(int i=0; i<bills.size(); i++){
+            if(bills[i] == 5){
+                cnt5++;
+            } 
+            else if(bills[i] == 10){
+               if (cnt5 == 0) return false;
+                cnt5--;
+                cnt10++;
+            }
+            else{
+                if (cnt10 > 0 && cnt5 > 0) {
+                    cnt10--;
+                    cnt5--;
+                } else if (cnt5 >= 3) {
+                    cnt5 -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
