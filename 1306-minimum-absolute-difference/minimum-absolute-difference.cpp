caf0ad938bfa300d56eec7c77d137f84c0f6bc32
Shafaqun Nisa <nisashafaqun@gmail.com>
2026-01-27 00:10:16 +0530

    Time: 15 ms (76.77%) | Memory: 36.6 MB (64.03%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -1,16 +1,39 @@
+// class Solution {
+// public:
+//     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+//         vector<vector<int>> ans;
+//         sort(arr.begin(),arr.end());
+//         int diff = INT_MAX;
+
+//         for(int i = 0; i<arr.size()-1; i++){
+//             diff = min(diff,arr[i+1]-arr[i]);
+//         }
+
+//         for(int i = 0; i<arr.size()-1; i++){
+//             if(arr[i+1]-arr[i] == diff){
+//                 ans.push_back({arr[i],arr[i+1]});
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
         vector<vector<int>> ans;
         sort(arr.begin(),arr.end());
-        int diff = INT_MAX;
+        int minDiff = INT_MAX;
 
         for(int i = 0; i<arr.size()-1; i++){
-            diff = min(diff,arr[i+1]-arr[i]);
-        }
+            int diff = arr[i+1]-arr[i];
 
-        for(int i = 0; i<arr.size()-1; i++){
-            if(arr[i+1]-arr[i] == diff){
+            if(minDiff > diff){
+                minDiff = diff;
+                ans = {};
+                ans.push_back({arr[i],arr[i+1]});
+            }
+            else if(minDiff == diff){
                 ans.push_back({arr[i],arr[i+1]});
             }
         }
