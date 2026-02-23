03ddbbcdf2866349b10e0ae4beb523819d5c86ea
Shafaqun Nisa <nisashafaqun@gmail.com>
2026-02-24 03:24:58 +0530

    Time: 455 ms (41.47%) | Memory: 119.1 MB (59.79%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,16 +1,32 @@
+// class Solution {
+// public:
+//     bool hasAllCodes(string s, int k) {
+//         int distinct_str = 1<<k;
+//         unordered_map<string,int> mp;
+       
+//         for(int i = 0; i+k <= s.size(); i++){
+//             string sub = "";
+//             for(int j = i; j<i+k; j++){
+//                 sub += s[j]; 
+//             }
+//             mp[sub]++;
+//         }
+//         return mp.size() == distinct_str;
+//     }
+// };
+
+
+///---------    METHOD 2 -------------
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        int distinct_str = 1<<k;
-        unordered_map<string,int> mp;
-       
-        for(int i = 0; i+k <= s.size(); i++){
-            string sub = "";
-            for(int j = i; j<i+k; j++){
-                sub += s[j]; 
-            }
-            mp[sub]++;
+        unordered_set<string> st;
+         int distinct_str = 1<<k;
+
+        for (int i = 0; i + k <= s.size(); i++) {
+            st.insert(s.substr(i, k));
         }
-        return mp.size() == distinct_str;
+
+         return st.size() == distinct_str;
     }
 };
