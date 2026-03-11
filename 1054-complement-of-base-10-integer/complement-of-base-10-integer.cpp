33e274584a3c43a18e424062026f8e8c365dbe5d
Shafaqun Nisa <nisashafaqun@gmail.com>
2026-03-12 00:16:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (26.22%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -2,11 +2,26 @@ class Solution {
 public:
     int bitwiseComplement(int n) {
         if(n == 0) return 1;
-        int mask = 1;
-
-        while(mask <= n)
-            mask <<= 1;
-
-        return (mask - 1) ^ n;
+        int ans = 0, pos = 0;
+        while(n>0){
+            int bit = (n&1)^1;
+            ans |= (bit << pos);
+            n >>= 1;
+            pos++;
+        }
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int bitwiseComplement(int n) {
+//         if(n == 0) return 1;
+//         int mask = 1;
+
+//         while(mask <= n)
+//             mask <<= 1;
+
+//         return (mask - 1) ^ n;
+//     }
+// };
