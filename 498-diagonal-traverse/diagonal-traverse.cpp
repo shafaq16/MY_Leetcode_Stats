b7635b7ff52a845c7c0a2fdc7be4c46f2bdc3b7e
Shafaqun Nisa <nisashafaqun@gmail.com>
2026-04-04 12:14:07 +0530

    Time: 10 ms (40.34%) | Memory: 25.7 MB (40.07%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -3,21 +3,46 @@ public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         int n = mat.size(), m = mat[0].size();
         vector<int> ans;
-        vector<vector<int>> diag (n+m-1);
-        for(int i = 0; i<n ; i++){
-            for(int j = 0; j<m ; j++){
-                diag[i+j].push_back(mat[i][j]);
+        for(int d = 0 ; d<n+m-1; d++){
+            vector<int> temp;
+            int r = (d<m) ? 0 : d-m+1;
+            int c = (d<m) ? d : m-1;
+            while(r<n && c>=0){
+                temp.push_back(mat[r][c]);
+                r++; c--;
             }
-        }
-
-        for(int d = 0; d<m+n-1; d++){
             if(d%2 == 0){
-                reverse(diag[d].begin(), diag[d].end());
+                reverse(temp.begin(),temp.end());
             }
-            for(int val : diag[d]){
+            for(int val: temp){
                 ans.push_back(val);
             }
         }
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+//         int n = mat.size(), m = mat[0].size();
+//         vector<int> ans;
+//         vector<vector<int>> diag (n+m-1);
+//         for(int i = 0; i<n ; i++){
+//             for(int j = 0; j<m ; j++){
+//                 diag[i+j].push_back(mat[i][j]);
+//             }
+//         }
+
+//         for(int d = 0; d<m+n-1; d++){
+//             if(d%2 == 0){
+//                 reverse(diag[d].begin(), diag[d].end());
+//             }
+//             for(int val : diag[d]){
+//                 ans.push_back(val);
+//             }
+//         }
+//         return ans;
+//     }
+// };
