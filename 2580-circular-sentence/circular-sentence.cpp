b2d0f76e6d09a2701e7334073810ec98eb4efe8f
Shafaqun Nisa <nisashafaqun@gmail.com>
2026-05-15 04:19:44 +0530

    Time: 3 ms (13.27%) | Memory: 9 MB (91.99%) - LeetSync

---
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        int n = sentence.size();
+        if(sentence[n-1] != sentence[0]) return false;
+        for(int i = 0; i<n; i++){
+            if(sentence[i] == ' '){
+                if(sentence[i-1] != sentence[i+1]) return false;
+            }
+        }
+        return true;
+    }
+};
