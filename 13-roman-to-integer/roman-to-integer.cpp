9ddfce6c4f24562b2388b8ff1c0e7135212b295c
Shafaqun Nisa <nisashafaqun@gmail.com>
2026-05-16 23:43:56 +0530

    Time: 12 ms (18.54%) | Memory: 13.5 MB (6.65%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,32 +1,57 @@
+// class Solution {
+// public:
+//     int romanToInt(string s) {
+//         int n = s.size();
+//         int sum = 0;
+//         for(int i = n-1; i>=0 ; i--){
+//             if(s[i]=='I'){
+//                 if(i!=n && (s[i+1] == 'V' || s[i+1] == 'X')){
+//                     sum -= 1;
+//                 }
+//                 else sum += 1;
+//             }
+//             else if(s[i] == 'V') sum += 5;
+//             else if(s[i] == 'X'){
+//                 if(i!=n && (s[i+1] == 'L' || s[i+1] == 'C')){
+//                     sum -= 10;
+//                 }
+//                 else sum += 10;
+//             }
+//             else if(s[i] == 'L') sum += 50;
+//             else if(s[i] == 'C'){
+//                 if(i!=n && (s[i+1] == 'D' || s[i+1] == 'M')){
+//                     sum -= 100;
+//                 }
+//                 else sum += 100;
+//             }
+//             else if(s[i] == 'D') sum += 500;
+//             else sum += 1000;
+//         }
+//         return sum;
+//     }
+// };
 class Solution {
 public:
     int romanToInt(string s) {
-        int n = s.size();
-        int sum = 0;
-        for(int i = n-1; i>=0 ; i--){
-            if(s[i]=='I'){
-                if(i!=n && (s[i+1] == 'V' || s[i+1] == 'X')){
-                    sum -= 1;
-                }
-                else sum += 1;
+        int res = 0;
+        unordered_map<char, int> roman = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10}, 
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (roman[s[i]] < roman[s[i + 1]]) {
+                res -= roman[s[i]];
+            } else {
+                res += roman[s[i]];
             }
-            else if(s[i] == 'V') sum += 5;
-            else if(s[i] == 'X'){
-                if(i!=n && (s[i+1] == 'L' || s[i+1] == 'C')){
-                    sum -= 10;
-                }
-                else sum += 10;
-            }
-            else if(s[i] == 'L') sum += 50;
-            else if(s[i] == 'C'){
-                if(i!=n && (s[i+1] == 'D' || s[i+1] == 'M')){
-                    sum -= 100;
-                }
-                else sum += 100;
-            }
-            else if(s[i] == 'D') sum += 500;
-            else sum += 1000;
         }
-        return sum;
+
+        return res + roman[s[s.size() - 1]];        
     }
 };
