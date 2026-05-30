8667de5cadb83f317f47c92bbd42748878e3bd70
Shafaqun Nisa <nisashafaqun@gmail.com>
2026-05-30 13:28:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (72.42%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -3,13 +3,12 @@ public:
     string reverseWords(string s) {
         int n = s.size();
         int j = 0;
-        for(int i = 0; i<n; i++){
-            if(s[i] == ' '){
+        for(int i = 0; i<=n; i++){
+            if(i==n || s[i] == ' '){
                 reverse(s.begin()+j, s.begin()+i);
                 j = i+1;
             }
         }
-        reverse(s.begin()+j, s.end());
         return s;
     }
 };
