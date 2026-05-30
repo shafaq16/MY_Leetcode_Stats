6f46f964208e2024c8bc44644d20feac8bea3136
Shafaqun Nisa <nisashafaqun@gmail.com>
2026-05-30 15:00:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (15.14%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.cpp
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -1,17 +1,31 @@
+// class Solution {
+// public:
+//     string reverseOnlyLetters(string s) {
+//         string temp;
+//         for(char ch: s){
+//             if(isalpha(ch)) temp += ch;
+//         }
+//         reverse(temp.begin(),temp.end());
+//         int j = 0, n = s.size();
+//         for(int i = 0; i< n ; i++){
+//             if(isalpha(s[i])){
+//                 s[i] = temp[j];
+//                 j++;
+//             }
+//         }
+//         return s;
+//     }
+// };
+
 class Solution {
 public:
     string reverseOnlyLetters(string s) {
-        string temp;
-        for(char ch: s){
-            if(isalpha(ch)) temp += ch;
-        }
-        reverse(temp.begin(),temp.end());
-        int j = 0, n = s.size();
-        for(int i = 0; i< n ; i++){
-            if(isalpha(s[i])){
-                s[i] = temp[j];
-                j++;
-            }
+        int i = 0, j = s.size() - 1;
+        while(i < j) {
+            while(i < j && !isalpha(s[i])) i++;
+            while(i < j && !isalpha(s[j])) j--;
+            swap(s[i], s[j]);
+            i++; j--;
         }
         return s;
     }
