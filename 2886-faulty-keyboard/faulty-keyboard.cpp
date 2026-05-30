8868d93534a5af60b2b19e1e83a539e1e0e9d189
Shafaqun Nisa <nisashafaqun@gmail.com>
2026-05-30 14:38:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (30.16%) - LeetSync

--- 2886-faulty-keyboard/faulty-keyboard.cpp
+++ 2886-faulty-keyboard/faulty-keyboard.cpp
@@ -3,11 +3,8 @@ public:
     string finalString(string s) {
         string ans = "";
         for(int i = 0; i<s.length(); i++){
-            if(s[i] == 'i'){
-                reverse(ans.begin(),ans.end());
-                continue;
-            }
-            ans += s[i];
+            if(s[i] == 'i') reverse(ans.begin(),ans.end());
+            else ans += s[i];
         }
         return ans;
     }
