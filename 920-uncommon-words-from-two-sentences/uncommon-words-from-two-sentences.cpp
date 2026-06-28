674482e5602cad8b8463bdf54b2c0a64b383fe68
Shafaqun Nisa <nisashafaqun@gmail.com>
2026-06-29 00:20:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (71.66%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
+        string s = s1 + " " + s2;
         unordered_map<string,int> mp;
-        addWords(s1, mp);
-        addWords(s2, mp);
+        string w = "";
+        for(char c : s) {
+            if(c == ' ') {
+                mp[w]++;
+                w = "";
+            } else {
+                w += c;
+            }
+        }
+        mp[w]++;
+        // if(w != "") mp[w]++;
         vector<string> ans;
         for(auto &p : mp) {
             if(p.second == 1)
@@ -11,17 +21,34 @@ public:
         }
         return ans;
     }
-private:
-    void addWords(string &s, unordered_map<string,int> &mp) {
-        string word = "";
-        for(char c : s) {
-            if(c == ' ') {
-                mp[word]++;
-                word = "";
-            } else {
-                word += c;
-            }
-        }
-        mp[word]++;
-    }
 };
+
+
+//
+//class Solution {
+// public:
+//     vector<string> uncommonFromSentences(string s1, string s2) {
+//         unordered_map<string,int> mp;
+//         addWords(s1, mp);
+//         addWords(s2, mp);
+//         vector<string> ans;
+//         for(auto &p : mp) {
+//             if(p.second == 1)
+//                 ans.push_back(p.first);
+//         }
+//         return ans;
+//     }
+// private:
+//     void addWords(string &s, unordered_map<string,int> &mp) {
+//         string word = "";
+//         for(char c : s) {
+//             if(c == ' ') {
+//                 mp[word]++;
+//                 word = "";
+//             } else {
+//                 word += c;
+//             }
+//         }
+//         mp[word]++;
+//     }
+// };
